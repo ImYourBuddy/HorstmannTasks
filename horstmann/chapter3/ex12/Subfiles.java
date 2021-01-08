e09c90eff28f46af68bb61fc78971cef6eb06f39
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-08 21:04:03 +0300

    Update Subfiles.java

--- horstmann/chapter3/ex12/Subfiles.java
+++ horstmann/chapter3/ex12/Subfiles.java
@@ -3,8 +3,13 @@ package com.mycompany.horstmann.chapter3.ex12;
 import java.io.File;
 
 public class Subfiles {
-    public static String[] getFiles(File file, String extension) {
-        return file.list(((dir, name) -> name.endsWith(extension)));
+    public static File[] getFiles(File file, String extension) {
+        String[] fileNames = file.list(((dir, name) -> name.endsWith(extension)));
+        File[] files = new File[fileNames.length];
+        for (int i = 0; i < fileNames.length; i++) {
+            files[i] = new File(file.getParent() + "\\" + file.getName() + "\\" + fileNames[i]);
+        }
+        return files;
     }
 }
 
