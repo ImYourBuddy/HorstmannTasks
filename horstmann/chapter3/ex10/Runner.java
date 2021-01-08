eeb1694ab536522b3cbfd7c26a72733b65520df2
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-08 20:38:32 +0300

    Update Runner.java

--- horstmann/chapter3/ex10/Runner.java
+++ horstmann/chapter3/ex10/Runner.java
@@ -3,7 +3,7 @@ package com.mycompany.horstmann.chapter3.ex10;
 public class Runner {
     public static void runTogether(Runnable... tasks) {
         for (Runnable task : tasks) {
-            new Thread(task).run();
+            new Thread(task).start();
         }
     }
 
