08c5e4a2644497bd44fc70f63c3591b2cb79b565
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-08 19:21:21 +0300

    Update Measurable.java

--- horstmann/chapter3/ex1andex2/Measurable.java
+++ horstmann/chapter3/ex1andex2/Measurable.java
@@ -2,4 +2,6 @@ package com.mycompany.horstmann.chapter3.ex1andex2;
 
 public interface Measurable {
     double getMeasure();
+    double average(Measurable[] objects);
+    Measurable largest(Measurable[] objects);
 }
