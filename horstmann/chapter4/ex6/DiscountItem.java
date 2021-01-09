68b755bc41beaa5cce3ecd5247f4faf35df53249
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-09 18:15:18 +0300

    Update DiscountItem.java

--- horstmann/chapter4/ex6/DiscountItem.java
+++ horstmann/chapter4/ex6/DiscountItem.java
@@ -13,8 +13,10 @@ public class DiscountItem extends Item {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || !(o instanceof DiscountItem)) return false;
+        if (o == null) return false;
         if (!super.equals(o)) return false;
+        if (Item.class == o.getClass()) return true;
+        if (!(getClass() == o.getClass())) return false;
         DiscountItem that = (DiscountItem) o;
         boolean discountEquals = (Math.abs(discount - that.discount) < 0.000001);
         return discountEquals;
@@ -26,21 +28,5 @@ public class DiscountItem extends Item {
         result = super.hashCode();
         result = 31 * result + Double.hashCode(discount);
         return result;
-    }
-
-    public static void main(String[] args) {
-        Item item1 = new Item("Test item", 10.0);
-        Item item2 = new Item("Test item", 10.0);
-        DiscountItem discountItem1 = new DiscountItem("Test item",10.0, 0.1);
-        DiscountItem discountItem2 = new DiscountItem("Test item",10.0, 0.1);
-        //Symmetry
-        System.out.println(item1.equals(item2));
-        System.out.println(item2.equals(item1));
-        System.out.println(discountItem1.equals(discountItem2));
-        System.out.println(discountItem2.equals(discountItem1));
-        //Transitive
-        System.out.println(item1.equals(discountItem1));
-        System.out.println(discountItem1.equals(discountItem2));
-        System.out.println(item1.equals(discountItem2));
     }
 }
