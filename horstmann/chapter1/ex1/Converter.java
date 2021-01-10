326c19f33cd4b66075d011287fa4492c471f123b
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-10 17:21:46 +0300

    Update Converter.java

--- horstmann/chapter1/ex1/Converter.java
+++ horstmann/chapter1/ex1/Converter.java
@@ -5,9 +5,11 @@ public class Converter {
         String binary = "0b" + Integer.toBinaryString(number);
         String octal = "0" + Integer.toOctalString(number);
         String hex = "0x" + Integer.toHexString(number);
+        String reciprocal = Double.toHexString(1.0 / number);
         System.out.println("Decimal to binary: " + binary);
         System.out.println("Decimal to octal: " + octal);
         System.out.println("Decimal to hex: " + hex);
+        System.out.println("Reciprocal to  hexadecimal floating-point: " + reciprocal);
     }
 
 }
