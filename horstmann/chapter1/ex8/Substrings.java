54f95940f482b23c1b677b428ab86e1261f1665a
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-11 17:21:36 +0300

    Update Substrings.java

--- horstmann/chapter1/ex8/Substrings.java
+++ horstmann/chapter1/ex8/Substrings.java
@@ -3,15 +3,26 @@ package com.mycompany.horstmann.chapter1.ex8;
 public class Substrings {
     /**
      * Prints all of its nonempty substrings.
+     *
      * @param s
      */
+//    public static void printSubstrings(String s) {
+//        String[] substrings = s.split("[ \\n\\t\\r]");
+//        for (String string : substrings) {
+//            if (string.equals("")) {
+//                continue;
+//            } else {
+//                System.out.println(string);
+//            }
+//        }
+//    }
+    //second implementation
     public static void printSubstrings(String s) {
-        String[] substrings = s.split("[ \\n\\t\\r]");
-        for (String string : substrings) {
-            if (string.equals("")) {
-                continue;
-            } else {
-                System.out.println(string);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if (!(s.substring(i, j + 1).equals("") || s.substring(i, j + 1).equals(s))) {
+                    System.out.println(s.substring(i, j + 1));
+                }
             }
         }
     }
