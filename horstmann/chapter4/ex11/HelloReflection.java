2c0a3d7f039c495d31d22969b447ca4c421463a8
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-17 21:11:30 +0300

    Update HelloReflection.java

--- horstmann/chapter4/ex11/HelloReflection.java
+++ horstmann/chapter4/ex11/HelloReflection.java
@@ -1,32 +1,20 @@
 package com.mycompany.horstmann.chapter4.ex11;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class HelloReflection {
     public static void main(String[] args) {
         Field field = null;
-        try {
-            field = System.class.getField("out");
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
         Method method = null;
         try {
-            method = field.getType().getMethod("println", String.class);
-        } catch (NoSuchMethodException e) {
+            field = System.class.getField("out");
+            method = field == null ? null : field.getType().getMethod("println", String.class);
+            if (method != null) {
+                method.invoke(field.get(null), "Hello, World!");
+            }
+        } catch (ReflectiveOperationException e) {
             e.printStackTrace();
         }
-        if (method != null) {
-            try {
-                method.invoke(field.get(null), "Hello, World!");
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-
     }
 }
